6bdbc7ee2dbc228c489b7a233fd907983fdf418e
Franco-BP <francobascialla@gmail.com>
2023-11-16 14:13:14 -0300

    Changes to routing

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
-import { Navigation } from 'react-native-navigation';
+import { StyleSheet } from 'react-native';
 import HomeLayout from './components/Layouts/HomeLayout';
 import RestaurantLayout from './components/Layouts/RestaurantLayout';
 import SearchLayout from './components/Layouts/SearchLayout';
@@ -15,76 +14,27 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <View style={styles.container}>
-          <SafeAreaView style={styles.container}>
-            <StoreProvider>
-              <Stack.Screen name='Home' component={<HomLayout/>} />
-              <Stack.Screen name='Search' component={<SearchLayout/>} />
-              <Stack.Screen name='Restaurant' component={<RestaurantLayout/>} />
-              <Stack.Screen name='Show' component={<ShowLayout/>} />
-            </StoreProvider>
-          </SafeAreaView>
-          <StatusBar style="auto" />
-        </View>
+        <StoreProvider>
+          <Stack.Screen name='Home' component={<HomeLayout/>} />
+          <Stack.Screen name='Search' component={<SearchLayout/>} />
+          <Stack.Screen name='Restaurant' component={<RestaurantLayout/>} />
+          <Stack.Screen name='Show' component={<ShowLayout/>} />
+        </StoreProvider>
       </Stack.Navigator>
     </NavigationContainer>
   )
 }
 
-export default App
-
-Navigation.events().registerAppLaunchedListener(async () => {
-  Navigation.setRoot({
-    root: {
-      stack: {
-        children: [
-          {
-            component: {
-              name: 'Home'
-            }
-          },
-        ]
-      },
-    }
+  // Componenets Style.
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      flexWrap: 'wrap',
+      backgroundColor: '#f3f6f4',
+      alignItems: 'center',
+      justifyContent: 'center',
+      marginTop: StatusBar.currentHeight || 0,
+    },
   });
-});
 
-// HomeScreen component.
-const HomeScreen = (props) => {
-  return (
-    <View style={styles.container}>
-      <SafeAreaView style={styles.container}>
-        <StoreProvider>
-          <HomeLayout componentId={props.componentId} />
-        </StoreProvider>
-      </SafeAreaView>
-      <StatusBar style="auto" />
-    </View>
-  );
-};
-
-// Starting options for the HomeScreen component.
-HomeScreen.options = {
-  topBar: {
-    title: {
-      text: 'Guía Rest'
-    }
-  }
-};
-
-Navigation.registerComponent('Home', () => HomeScreen);
-Navigation.registerComponent('Search', () => {<SearchLayout />});
-Navigation.registerComponent('Restaurant', () => {<RestaurantLayout />});
-Navigation.registerComponent('Show', () => {<ShowLayout />});
-
-// Componenets Style.
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexWrap: 'wrap',
-    backgroundColor: '#f3f6f4',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: StatusBar.currentHeight || 0,
-  },
-})
+export default App;
