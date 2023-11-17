9119b4f9f115d7629bd491870b60baf48093532c
Franco-BP <francobascialla@gmail.com>
2023-11-17 11:47:11 -0300

    Changes 11

--- components/SearchBarButton.jsx
+++ components/SearchBarButton.jsx
@@ -11,18 +11,18 @@ const SearchBarButton = ({ restaurants, navigation }) => {
 
   const styles = StyleSheet.create({
     searchBarContainer: {
-      height: 20,
+      minHeight: 20,
       marginVertical: 15,
       paddingHorizontal: 40,
       width: dimensionsWidth-40,
+    },
+    input: {
+      padding: 10,
       borderWidth: 1,
       borderRadius: 10,
       flexDirection: 'row',
       alignContent: 'center',
       justifyContent: 'flex-start',
-    },
-    input: {
-      // padding: 10,
     },
     searchIcon: {
       height: 15,
@@ -39,10 +39,10 @@ const SearchBarButton = ({ restaurants, navigation }) => {
       style={styles.searchBarContainer}
       onPress={() => {() => handleNavigation()}}
     >
+      <View style={styles.input}>
         <Image style={styles.searchIcon} source={{uri: SEARCH_ICON}} />
         <Text style={styles.placeHolder}>Buscar por nombre...</Text>
-      {/* <View style={styles.input}>
-      </View> */}
+      </View>
     </TouchableOpacity>
   )
 }
